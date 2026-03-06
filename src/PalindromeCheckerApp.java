17a2883596d50e0cf8ff373ad14e61342d3a6b3c
Harshavarthansb <hs2625@srmist.edu.in>
2026-03-06 10:35:51 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,22 @@
 
 public class PalindromeCheckerApp {
 
-    /**
-     * Application entry point.
-     * This method is executed first when the program starts.
-     */
-
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome?: " + isPalindrome);
 
     }
 }
