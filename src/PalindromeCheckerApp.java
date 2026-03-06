9650b69273e687b629f70b92df757c9c0acd0733
Harshavarthansb <hs2625@srmist.edu.in>
2026-03-06 10:14:11 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,5 @@
+public class PalindromeCheckerApp {
+}
+/*git config --global user.name "Harshavarthansb"
+git config --global user.email "hs2625@srmist.edu.in"
+git remote add originhttps://github.com/Harshavarthansb/-PalindromeCheckerApp
